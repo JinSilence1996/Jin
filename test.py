3dc184c470564294822ed45435e9531d91f5974b
JinSilence1996 <2693279042@qq.com>
2020-05-11 02:25:03 +0800

    修改py

--- test.py
+++ test.py
@@ -52,11 +52,11 @@ def test_request():
         'Connection': 'keep-alive',
         'Cache-Control': 'max-age=0',
         'Upgrade-Insecure-requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' + \
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' + \
         'Chrome/84.0.4133.0 Safari/537.36 Edg/84.0.508.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
-        'image/webp,image/apng,*/*;'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,' + \
+        'image/webp,image/apng,*/*;' + \
         'q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'navigate',
